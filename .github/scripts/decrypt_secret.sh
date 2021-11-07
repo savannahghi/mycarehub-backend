4a85be5e31ae75a831fd0bf249a63c5fa4314852
Ngure Nyaga <ngure.nyaga@savannahinformatics.com>
2021-11-07 14:28:55 +0300

    chore: update Github actions

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -5,3 +5,8 @@ mkdir $GITHUB_WORKSPACE/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --verbose --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" --output $GITHUB_WORKSPACE/secrets/gcs.json gcs.json.gpg
+
+# for easier troubleshooting
+cd $GITHUB_WORKSPACE/secrets
+pwd
+ls -al
