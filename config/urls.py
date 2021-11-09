550d3b6d966dde7784aa58aabbaef7ad91a1f064
Ngure Nyaga <ngure.nyaga@savannahinformatics.com>
2021-11-09 07:44:14 +0300

    feat: add /openapi (OAS schema) and schema docs (#17)
    This will make the REST API self describing

--- config/urls.py
+++ config/urls.py
@@ -6,9 +6,10 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path, re_path
 from django.views import defaults as default_views
 from django.views.decorators.csrf import csrf_exempt
-from django.views.generic import RedirectView
+from django.views.generic import RedirectView, TemplateView
 from graphene_django.views import GraphQLView
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework.schemas import get_schema_view
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
@@ -45,7 +46,7 @@ urlpatterns = [
     # re_path(r"authoring", include("mycarehub.content.urls")),
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's serving mechanism
-    re_path(r"content", include(wagtail_urls)),
+    re_path(r"content", include(wagtail_urls), name="wagtail"),
     path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True)), name="graphql"),
     # content management
     path("admin/", include(wagtailadmin_urls)),
@@ -61,13 +62,42 @@ if settings.DEBUG:
     urlpatterns += staticfiles_urlpatterns()
 
 # API URLS
-urlpatterns += [
+api_patterns = [
     # API base url
     path("api/", include("config.api_router")),
     # DRF auth token
     path("auth-token/", obtain_auth_token),
     # OAuth 2
     path("o/", include("oauth2_provider.urls", namespace="oauth2_provider")),
+]
+urlpatterns += api_patterns
+
+# Open API Schema and automatic API documentation
+urlpatterns += [
+    path(
+        "openapi",
+        get_schema_view(
+            title=settings.SITE_NAME,
+            description=f"{settings.SITE_NAME} REST API",
+            version=settings.API_VERSION,
+            patterns=api_patterns,
+        ),
+        name="openapi-schema",
+    ),
+    path(
+        "swagger/",
+        TemplateView.as_view(
+            template_name="swagger-ui.html", extra_context={"schema_url": "openapi-schema"}
+        ),
+        name="swagger-ui",
+    ),
+    path(
+        "redoc/",
+        TemplateView.as_view(
+            template_name="redoc.html", extra_context={"schema_url": "openapi-schema"}
+        ),
+        name="redoc",
+    ),
 ]
 
 if settings.DEBUG:
