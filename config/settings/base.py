8198624fe40705ba76c24f66c8cff02c0889bd89
Charles Muchogo <dev@muchogo.com>
2022-03-10 12:17:37 +0300

    feat: appointment model (#97)

--- config/settings/base.py
+++ config/settings/base.py
@@ -121,6 +121,7 @@ LOCAL_APPS = [
     "mycarehub.authority.apps.AuthorityConfig",
     "mycarehub.communities.apps.CommunityConfig",
     "mycarehub.screeningtools.apps.ScreeningtoolsConfig",
+    "mycarehub.appointments.apps.AppointmentsConfig",
 ]
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
