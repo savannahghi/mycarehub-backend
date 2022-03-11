a57bb1693e783841aecec45838bbd37c669e5932
Charles Muchogo <dev@muchogo.com>
2022-03-11 12:46:09 +0300

    chore: add appointment status choices and shared appointment identifier (#98)

--- mycarehub/appointments/models.py
+++ mycarehub/appointments/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.enums import TextChoices
 from django.utils.translation import gettext_lazy as _
 
 from mycarehub.clients.models import Client
@@ -15,8 +16,25 @@ class Appointment(AbstractBase):
     It is referenced from the Open MRS appointment data model
     """
 
+    class AppointmentStatus(TextChoices):
+        SCHEDULED = "SCHEDULED", _("Scheduled")
+        RESCHEDULED = "RESCHEDULED", _("Rescheduled")
+        WAITING = "WAITING", _("Waiting")
+        MISSED = "MISSED", _("Missed")
+        COMPLETED = "COMPLETED", _("Completed")
+        INCONSULTATION = "INCONSULTATION", _("In Consultation")
+        WALKIN = "WALKIN", _("Walk In")
+        CANCELLED = "CANCELLED", _("Cancelled")
+        NEEDSRESCHEDULE = "NEEDSRESCHEDULE", _("Needs Reschedule")
+
+    appointment_uuid = models.UUIDField(
+        editable=False,
+        null=True,
+        blank=True,
+        help_text=_("Identifier that is shared between KenyaEMR and MyCareHub"),
+    )
     appointment_type = models.CharField(max_length=36)
-    status = models.CharField(max_length=36)
+    status = models.CharField(max_length=36, choices=AppointmentStatus.choices)
     reason = models.TextField(max_length=1024, null=True, blank=True)
     client = models.ForeignKey(Client, on_delete=models.PROTECT)
     staff = models.ForeignKey(Staff, on_delete=models.PROTECT, null=True, blank=True)
@@ -28,9 +46,9 @@ class Appointment(AbstractBase):
         null=True,
         blank=True,
     )
-    date = models.DateField()
-    start_time = models.TimeField()
-    end_time = models.TimeField()
+    date = models.DateField(null=True, blank=True)
+    start_time = models.TimeField(null=True, blank=True)
+    end_time = models.TimeField(null=True, blank=True)
 
     def __str__(self) -> str:
         return f"{self.client} - {self.appointment_type} - {self.status}"
