239bb309768903713416a5453cf34a2e9f76ba9a
maxwellgithinji <maxwellgithinji@gmail.com>
2022-03-19 11:39:16 +0300

    chore: add meta to service request
    Signed-off-by: maxwellgithinji <maxwellgithinji@gmail.com>

--- mycarehub/clients/models.py
+++ mycarehub/clients/models.py
@@ -443,4 +443,4 @@ class ServiceRequest(AbstractBase):
     resolved_at = models.DateTimeField(null=True, blank=True)
 
     facility = models.ForeignKey(Facility, null=True, blank=True, on_delete=models.SET_NULL)
-    ccc_number = models.CharField(max_length=36, null=True, blank=True)
+    meta = models.JSONField(null=True, blank=True)
