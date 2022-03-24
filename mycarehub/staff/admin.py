e56e3d31cddf851b8f5e8988489bfb60b7491bdc
Maxwell Githinji <maxwellgithinji@gmail.com>
2022-03-24 11:59:59 +0300

    chore: add staff service request to admin (#107)
    Signed-off-by: maxwellgithinji <maxwellgithinji@gmail.com>

--- mycarehub/staff/admin.py
+++ mycarehub/staff/admin.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 
 from mycarehub.common.admin import BaseAdmin
 
-from .models import Staff
+from .models import ServiceRequest, Staff
 
 
 @admin.register(Staff)
 class StaffAdmin(BaseAdmin):
+    pass
+
+
+@admin.register(ServiceRequest)
+class ServiceRequestAdmin(BaseAdmin):
     pass
