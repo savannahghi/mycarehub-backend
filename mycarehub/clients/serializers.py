7ae2c0fe352d8b4bd7b7ca8c3b9ac767b2798a7c
Charles Muchogo <dev@muchogo.com>
2022-09-02 12:37:23 +0300

    chore: client registration api (#150)

--- mycarehub/clients/serializers.py
+++ mycarehub/clients/serializers.py
@@ -1,7 +1,7 @@
 from django import forms
 from drf_braces.serializers.form_serializer import FormSerializer, make_form_serializer_field
 from phonenumber_field.formfields import PhoneNumberField
-from rest_framework.serializers import CharField, ListField, ModelSerializer
+from rest_framework.serializers import CharField, ListField, ModelSerializer, UUIDField
 
 from mycarehub.clients.models import (
     Caregiver,
@@ -66,6 +66,7 @@ class ClientRegistrationSerializer(FormSerializer):
         field_mapping = {
             PhoneNumberField: make_form_serializer_field(CharField),
             forms.MultipleChoiceField: make_form_serializer_field(ListField),
+            forms.UUIDField: make_form_serializer_field(UUIDField),
         }
 
 
