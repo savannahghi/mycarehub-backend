d2a421dd688d38fa550b607c6d8d27c3fe9ec1dd
Muchogo <dev@muchogo.com>
2023-02-15 12:56:54 +0300

    chore: show organisation programs in snippet creation form

--- mycarehub/content/models/snippets.py
+++ mycarehub/content/models/snippets.py
@@ -2,10 +2,17 @@ from django import forms
 from django.db import models
 from modelcluster.fields import ParentalManyToManyField
 from modelcluster.models import ClusterableModel
+from wagtail.admin.forms import WagtailAdminModelForm
 from wagtail.admin.panels import FieldPanel
 from wagtail.search import index
 
 from mycarehub.common.models import AbstractBase, Organisation, Program
+
+
+class CustomSnippetForm(WagtailAdminModelForm):
+    def __init__(self, *args, **kwargs):  # pragma: no cover
+        super().__init__(*args, **kwargs)
+        self.fields["programs"].queryset = self.for_user.organisation.common_program_related.all()
 
 
 class Author(AbstractBase, ClusterableModel):
@@ -33,12 +40,17 @@ class Author(AbstractBase, ClusterableModel):
         FieldPanel("programs", widget=forms.CheckboxSelectMultiple),
     ]
 
+    base_form_class = CustomSnippetForm
+
     def __str__(self):
         """Represent an author by their name."""
         return self.name
 
     def get_programs(self):
         return "\n".join([p.name for p in self.programs.all()])
+
+    get_programs.admin_order_field = "programs"  # type: ignore
+    get_programs.short_description = "Programs"  # type: ignore
 
     class Meta(AbstractBase.Meta):
         verbose_name_plural = "authors"
@@ -87,11 +99,16 @@ class ContentItemCategory(index.Indexed, ClusterableModel):
 
     search_fields = [index.SearchField("name"), index.FilterField("organisation_id")]
 
+    base_form_class = CustomSnippetForm
+
     def __str__(self):
         return self.name
 
     def get_programs(self):
         return "\n".join([p.name for p in self.programs.all()])
+
+    get_programs.admin_order_field = "programs"  # type: ignore
+    get_programs.short_description = "Programs"  # type: ignore
 
     class Meta:
         verbose_name_plural = "content item categories"
