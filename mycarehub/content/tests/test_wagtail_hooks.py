812fd89a0830db478ed0bbeda2388d265f92a011
Kathurima Kimathi <kathurimakimathi415@gmail.com>
2023-07-25 11:34:39 +0300

    fix: return form error messages - when the content item type is AUDIO_VIDEO or PDF_DOCUMENT, and the relevant media/document type is not attached,   return form error message rather than INTERNAL server error(500)
    Signed-off-by: Kathurima Kimathi <kathurimakimathi415@gmail.com>

--- mycarehub/content/tests/test_wagtail_hooks.py
+++ mycarehub/content/tests/test_wagtail_hooks.py
@@ -1,5 +1,4 @@
 import pytest
-from django.core.exceptions import ValidationError
 from django.utils import timezone
 from model_bakery import baker
 from wagtail.models import Page, Site
@@ -24,46 +23,6 @@ pytestmark = pytest.mark.django_db
 def test_get_global_admin_js():
     admin_script = get_global_admin_js()
     assert "DOMContentLoaded" in admin_script
-
-
-def test_before_publish_content_item_audio_video_no_media(
-    request_with_user,
-    content_item_with_tag_and_category,
-):
-    page = content_item_with_tag_and_category
-    page.item_type = "AUDIO_VIDEO"
-    page.save()
-    page.refresh_from_db()
-    assert page.featured_media.count() == 0
-    assert page.item_type == "AUDIO_VIDEO"
-
-    with pytest.raises(ValidationError) as c:
-        before_publish_page(request_with_user, page)
-
-    assert (
-        "an AUDIO_VIDEO content item must have at least one video "
-        "or audio file before publication"
-    ) in str(c.value.messages)
-
-
-def test_before_publish_content_item_pdf_document_no_document(
-    request_with_user,
-    content_item_with_tag_and_category,
-):
-    page = content_item_with_tag_and_category
-    page.item_type = "PDF_DOCUMENT"
-    page.save()
-    page.refresh_from_db()
-    assert page.documents.count() == 0
-    assert page.item_type == "PDF_DOCUMENT"
-
-    with pytest.raises(ValidationError) as c:
-        before_publish_page(request_with_user, page)
-
-    assert (
-        "a PDF_DOCUMENT content item must have at least one document "
-        "attached before publication"
-    ) in str(c.value.messages)
 
 
 def test_before_publish_non_content_item_page(request_with_user):
